f49b45849694fd441d2ee743290351a8f4ea46be
qiengo <ewangzhihua@yeah.net>
2015-05-09 19:47:26 +0800

    sun

--- src/com/wzh/logistics/activity/HuodanDetailActivity.java
+++ src/com/wzh/logistics/activity/HuodanDetailActivity.java
@@ -9,5 +9,6 @@ public class HuodanDetailActivity extends ActionBarBaseActivity{
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_huodandetail);
+		setActionBarTitel("货单详情");
 	}
 }
