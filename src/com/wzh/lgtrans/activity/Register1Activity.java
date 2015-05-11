0b2ba237982a80db044c75d60275bf2ddc53cbc7
qiengo <ewangzhihua@yeah.net>
2015-05-11 17:39:25 +0800

    tt

---
+++ src/com/wzh/lgtrans/activity/Register1Activity.java
@@ -0,0 +1,14 @@
+package com.wzh.lgtrans.activity;
+
+import android.os.Bundle;
+
+import com.wzh.lgtrans.R;
+
+public class Register1Activity extends ActionBarBaseActivity{
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_register0);
+		setActionBarTitel("注册");
+	}
+}
---
+++ src/com/wzh/lgtrans/fragment/MoreFragment.java
@@ -0,0 +1,56 @@
+package com.wzh.lgtrans.fragment;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.wzh.lgtrans.R;
+import com.wzh.lgtrans.activity.MyInfoActivity;
+
+/**
+ * 购物车界面的fragment
+ * 
+ * @author 王植桦 ewangzhihua@yeah.net
+ * @version 创建时间：2014年3月19日
+ *
+ */
+public class MoreFragment extends Fragment {
+	public static final String TAG = "MainFragment";
+	/**
+	 * 实例化布局的对象
+	 */
+	private LayoutInflater mInflater;
+	private Context ctx;
+
+	@Override
+	public void onAttach(Activity activity) {
+		super.onAttach(activity);
+		ctx = (Context) activity;
+	}
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		mInflater = LayoutInflater.from(ctx);
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		View mainView = mInflater.inflate(R.layout.fragment_more, container, false);
+//		mainView.findViewById(R.id.lay_lin_myinfo).setOnClickListener(new View.OnClickListener() {
+//			
+//			@Override
+//			public void onClick(View v) {
+//				Intent intent=new Intent(ctx, MyInfoActivity.class);
+//				ctx.startActivity(intent);
+//			}
+//		});
+		return mainView;
+	}
+
+}
