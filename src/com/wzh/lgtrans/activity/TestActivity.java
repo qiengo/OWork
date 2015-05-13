1f0e54ffa188d9a73aec71e40961cb76e160f512
qiengo <ewangzhihua@yeah.net>
2015-05-13 17:28:10 +0800

    tt

---
+++ src/com/wzh/lgtrans/activity/TestActivity.java
@@ -0,0 +1,31 @@
+package com.wzh.lgtrans.activity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.wzh.lgtrans.R;
+import com.wzh.lgtrans.dialog.CityPickerDialog;
+
+public class TestActivity extends ActionBarBaseActivity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_test);
+		findViewById(R.id.btn_test_pop).setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				new CityPickerDialog(TestActivity.this){
+
+					@Override
+					public void onConfirm(String ret) {
+						dismiss();
+						Log.i("tt", ret);
+					}
+					
+				}.show();
+			}
+		});
+	}
+}
---
+++ src/com/wzh/lgtrans/dialog/CityPickerDialog.java
@@ -0,0 +1,31 @@
+package com.wzh.lgtrans.dialog;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.view.View;
+
+import com.wzh.lgtrans.R;
+
+public class CityPickerDialog extends Dialog {
+	private String ret="asdf";
+
+	public CityPickerDialog(final Context context) {
+		super(context);
+		setTitle("选择城市:");
+		setContentView(R.layout.dialog_citypicker);
+		setCanceledOnTouchOutside(true);
+
+		findViewById(R.id.btn_citypicker_ok).setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				onConfirm(ret);
+			}
+		});
+	}
+
+	public void onConfirm(String ret) {
+		dismiss();
+	}
+
+}
